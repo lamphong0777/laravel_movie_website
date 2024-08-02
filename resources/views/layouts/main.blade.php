db38065992debc028b61bf85bfba879687d182e1
nghiab2014765 <lamphong0777@gmail.com>
2024-08-02 23:56:34 +0700

    create testing API and search dropdown successfully

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,6 +13,7 @@
 
     <!-- Styles -->
     @vite('resources/css/app.css')
+    @livewireStyles
 
 {{--    <link rel="stylesheet" href="./css/main.css">--}}
 </head>
@@ -37,12 +38,7 @@
             </li>
         </ul>
         <div class="flex items-center">
-            <div class="relative">
-                <input type="text" class="bg-gray-800 text-small rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="search">
-                <div class="absolute top-0">
-                    <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
-                </div>
-            </div>
+            <livewire:search-dropdown />
             <div class="md:ml-4 mt-3 md:mt-0">
                 <a href="#">
                     <img src="{{ asset('img/avatar.jpg') }}" alt="avatar" class="rounded-full w-8 h-8">
@@ -52,5 +48,6 @@
     </div>
 </nav>
 @yield('content')
+@livewireScripts
 </body>
 </html>
